186dec054823eaeb931d0ba6cb0222299e01e9cb
Shiomi <shiomi.hoshiya@gmail.com>
2023-07-25 13:33:11 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import Galery from './pages/Galery';
-import Contact from './pages/Contact';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Gallery from "./pages/Gallery";
+import Contact from "./pages/Contact";
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -15,11 +15,10 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/galery" element={<Galery />} />
+          <Route path="/gallery" element={<Gallery />} />
         </Routes>
       </div>
     </Router>
-
   );
 }
 
---
+++ src/pages/Gallery.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/gallery.css"
+const Gallery = () => {
+  return (
+    <section id="gallery">
+      <h1>Gallery</h1>
+
+    </section>
+  );
+};
+
+export default Gallery;
